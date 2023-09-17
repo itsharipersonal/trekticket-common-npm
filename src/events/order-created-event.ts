8960fefb771d7e101472809b55aa7570e4d9a0c0
Harikrishnan DS <thisismekrishnan@gmail.com>
2023-09-17 19:31:43 +0530

    Updates

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -1,7 +1,7 @@
 import { Subjects } from "./subjects";
 import { OrderStatus } from "./types/order-status";
 
-export interface OrderCreatedEvents {
+export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
