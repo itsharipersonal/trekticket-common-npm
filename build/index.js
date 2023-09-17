8cb965359167d72ff117ae4ebd6df463bb4f6600
Harikrishnan DS <thisismekrishnan@gmail.com>
2023-09-17 23:02:39 +0530

    adding order publish

--- build/index.js
+++ build/index.js
@@ -31,4 +31,4 @@ __exportStar(require("./events/ticket-created-event"), exports);
 __exportStar(require("./events/ticket-updated-event"), exports);
 __exportStar(require("./events/types/order-status"), exports);
 __exportStar(require("./events/order-created-event"), exports);
-__exportStar(require("./events/order-created-event"), exports);
+__exportStar(require("./events/order-cancelled-event"), exports);
