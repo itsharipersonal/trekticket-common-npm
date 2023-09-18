39e9e0eca8289226e892995eb01f621c98c92176
Harikrishnan DS <thisismekrishnan@gmail.com>
2023-09-18 16:06:47 +0530

    Updates

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -1,11 +1,12 @@
 import { Subjects } from "./subjects";
 
 export interface OrderCancelledEvent {
-    subject : Subjects.OrderCancelled;
-    data:{
-        id:string;
-        ticket:{
-            id:string;
-        }
-    }
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    version: number;
+    ticket: {
+      id: string;
+    };
+  };
 }
